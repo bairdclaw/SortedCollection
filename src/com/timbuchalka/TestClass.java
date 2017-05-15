77bde9ab650867f15fc0cb6d35ea825eea4dac22
Loren Baird <lornie@gmail.com>
2017-05-15 15:17:32 -0500

    Committing changes.  New method and new class.

---
+++ src/com/timbuchalka/TestClass.java
@@ -0,0 +1,12 @@
+package com.timbuchalka;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+		System.out.println("Yo mama!");
+
+	}
+
+}
